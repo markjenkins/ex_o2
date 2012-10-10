0621f0fcd893b983dc84268a4d970170514213e5
Mark Jenkins <mark@parit.ca>
2012-10-10 18:19:15 -0500

    grabbing points over an interval

--- o2_stat.py
+++ o2_stat.py
@@ -16,7 +16,20 @@ def gen_o2_stats_from_file(filename):
     with open(filename) as f:
         d = DictReader(f, )
         for line in d:
-            yield ( int(line[' Ch1rSO2']),
-                    int(line[' Ch2rSO2']) )
+            try:
+                r = int(line[' Ch1rSO2'])
+            except ValueError:
+                r = None
+
+            try:
+                l = int(line[' Ch2rSO2'])
+            except ValueError:
+                l = None
+
+            yield (r, l)
 
 
+def gen_points_over_interval(filename, start, end):
+    return ( points
+             for i, points in enumerate(gen_o2_stats_from_file(filename), 1)
+             if i>=start and i<=end )
