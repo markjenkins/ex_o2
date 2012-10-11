5c20b3948dffd93336ed88ee64d781893a81e328
Mark Jenkins <mark@parit.ca>
2012-10-10 21:50:29 -0500

    get rid of left/right convention, go with channels

--- ave_of_points.py
+++ ave_of_points.py
@@ -17,13 +17,13 @@ from o2_stat import gen_points_over_interval
 from stats import average
 
 def ave_over_interval(filename, start_row, end_row):
-    (r_ave, l_ave) = tuple(
+    (chan1_ave, chan2_ave) = tuple(
         average( points[i]
                  for points in
                  gen_points_over_interval(filename, start_row, end_row)
                  if points[i] != None )
         for i in range(1+1) )
-    return( r_ave, l_ave, average( (r_ave, l_ave) ) )
+    return( chan1_ave, chan2_ave, average( (chan1_ave, chan2_ave) ) )
 
 filename, start_row, end_row = argv[1], int(argv[2]), int(argv[3])
 print(ave_over_interval(filename, start_row, end_row) )
